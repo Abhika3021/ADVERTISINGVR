e269b0300d5fd101edfd218f2f0a4d2886ebf372
Abhika <abhikapkw@gmail.com>
2023-09-02 13:14:18 +0530

    Star Flex.. icons left

--- src/Components/Services/StarFlex.js
+++ src/Components/Services/StarFlex.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import HubTwoToneIcon from '@mui/icons-material/HubTwoTone';
+import { FaCloudSunRain } from 'react-icons/fa';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import EngineeringIcon from '@mui/icons-material/Engineering';
+import PersonalVideoIcon from '@mui/icons-material/PersonalVideo';
+import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh';
 
 const StarFlex = () => {
   const responsiveSettings = [
@@ -35,123 +41,231 @@ const StarFlex = () => {
     slidesToScroll: 1, // Number of slides to scroll
     responsive: responsiveSettings,
   };
+
+  const fullContent = `It refers to a specialized type of printable material that boasts exceptional durability and vivid color reproduction. Star flex is constructed from a resilient PVC , substrate, fortified with UV-resistant properties, rendering it ideal for outdoor applications such as billboards, banners, and hoardings. It has ability to withstand harsh weather conditions, including intense sunlight, heavy rains, and strong winds, without compromising print quality. The term "star" in star flex signifies its stellar performance in enduring the rigors of outdoor exposure while maintaining the brilliance of printed graphics and text. Due to its longevity and visual appeal, it is a preferred choice for large-format advertising campaigns, ensuring that messages and visuals remain striking and vibrant even under adverse environmental circumstances.`;
+
+  const [isMobileView, setIsMobileView] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  // Check if the screen width is less than a certain breakpoint (e.g., 768px)
+  const checkMobileView = () => {
+    setIsMobileView(window.innerWidth < 768);
+  };
+
+  // Add an event listener to check for screen width changes
+  React.useEffect(() => {
+    checkMobileView();
+    window.addEventListener("resize", checkMobileView);
+    return () => window.removeEventListener("resize", checkMobileView);
+  }, []);
+
   return (
     <div className='container'>
+      <div className="container advantages-slider mb-5">
+        <Slider {...settings}>
+          <div className="slide">
+
+          </div>
+          <div className="slide">
+
+          </div>
+          <div className="slide">
+
+          </div>
+          <div className="slide">
+
+          </div>
+          <div className="slide">
+
+          </div>
+        </Slider>
+      </div>
       <div className="flex-header">
         <div className="head text-center">
           <h2>
-            What is Star Flex?
+            Star Flex
           </h2>
         </div>
-        <div className="head-content">
+        <div className="head-content text-center">
           <p>
-            Made from PVC material, which is reinforced with a strong polyester mesh making it highly resistant to tearing, fading, and damage from weather elements such as rain, sunlight, and wind. The PVC coating also gives it a smooth and glossy surface that enhances the print quality and visual appeal of the final output.
+            {isMobileView && !isExpanded
+              ? `${fullContent.slice(0, 200)}...`
+              : fullContent}
+            {isMobileView && !isExpanded && (
+              <a
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setIsExpanded(true);
+                }}
+                className="read-more-link"
+              >
+                Read More
+              </a>
+            )}
+            {isMobileView && isExpanded && (
+              <a
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setIsExpanded(false);
+                }}
+                className="read-less-link"
+              >
+                Read Less
+              </a>
+            )}
           </p>
         </div>
       </div>
 
+
       <div className="flex-content">
         <div className="uses">
-          <span>
-            Uses of Star Flex in Flex Printing and Hoardings Business:
-          </span>
-          <ul>
-            <li>
-              <h3>
-                Outdoor Banners and Hoardings:
-              </h3>
-              <p>
-                ideal due to its robustness and ability to withstand outdoor conditions. It is commonly used for promotional banners, event advertisment
-              </p>
-            </li>
+          <div className="usesInner container-fluid">
+            <div className='usesHead'>
+              <div className='usesHeadTitle'>
+                <h2 class="usesTitle">
+                  What are the <span>uses</span> of <span>Star</span> Flex
+                </h2>
+              </div>
+            </div>
+            <div className="usesBody">
+              <ul>
+                <li>
+                  <h3 className='usesListItemTitle'>
+                    Outdoor Banners and Hoardings:
+                  </h3>
+                  <p>
+                    ideal due to its robustness and ability to withstand outdoor conditions. It is commonly used for promotional banners, event advertisment
+                  </p>
+                </li>
 
-            <li>
-              <h3>
-                Billboards:
-              </h3>
-              <p>
-                It's excellent durability ensures that billboards can withstand exposure to harsh weather conditions, which is crucial for long-term outdoor advertising.
-              </p>
-            </li>
+                <li>
+                  <h3 className='usesListItemTitle'>
+                    Billboards:
+                  </h3>
+                  <p>
+                    It's excellent durability ensures that billboards can withstand exposure to harsh weather conditions, which is crucial for long-term outdoor advertising.
+                  </p>
+                </li>
 
-            <li>
-              <h3>
-                Event Backdrops:
-              </h3>
-              <p>
-                During trade show, conference, or stage performance, star flex can be used to create visually striking backdrops that effectively promote brands or enhance the event's theme.
-              </p>
-            </li>
+                <li>
+                  <h3 className='usesListItemTitle'>
+                    Event Backdrops:
+                  </h3>
+                  <p>
+                    During trade show, conference, or stage performance, star flex can be used to create visually striking backdrops that effectively promote brands or enhance the event's theme.
+                  </p>
+                </li>
 
-            <li>
-              <h3>
-                Building Wraps:
-              </h3>
-              <p>
-                Its size and durability make it suitable for Large-scale advertisements on the facades of buildings  covering substantial surfaces.
-              </p>
-            </li>
+                <li>
+                  <h3 className='usesListItemTitle'>
+                    Building Wraps:
+                  </h3>
+                  <p>
+                    Its size and durability make it suitable for Large-scale advertisements on the facades of buildings  covering substantial surfaces.
+                  </p>
+                </li>
 
-            <li>
-              <h3>
-                Street Banners:
-              </h3>
-              <p>
-                oftenly used to display public messages, event announcements, or seasonal greetings on street poles or lamp posts.
-              </p>
-            </li>
+                <li>
+                  <h3 className='usesListItemTitle'>
+                    Street Banners:
+                  </h3>
+                  <p>
+                    oftenly used to display public messages, event announcements, or seasonal greetings on street poles or lamp posts.
+                  </p>
+                </li>
 
-            <li>
-              <h3>
-                Vehicle Wraps:
-              </h3>
-              <p>
-                By turning cars, buses, or trucks into moving advertisements, reaching a broader audience while on the road.
-              </p>
-            </li>
-
-          </ul>
+                <li>
+                  <h3 className='usesListItemTitle'>
+                    Vehicle Wraps:
+                  </h3>
+                  <p>
+                    By turning cars, buses, or trucks into moving advertisements, reaching a broader audience while on the road.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
 
-        <div className='advantages'>
-          <h2>
-            Advantages of Star Flex in Flex Printing and Hoardings Business:
-          </h2>
+        <div className="why">
+          <div className="whyInner container-fluid">
+            <div className='whyHead'>
+              <div className='whyHeadTitle'>
+                <h2 className="whyTitle">
+                  What are the <span>Advantages</span> of using <span>Star</span> Flex
+                </h2>
+              </div>
+            </div>
 
-          <div className="container advantages-slider mb-5">
-            <Slider {...settings}>
-              <div className="slide">
-                <h4>Durability:</h4>
-                <p>
-                  Designed to withstand harsh environmental conditions like sunlight, rain, wind, and temperature variations without significant wear and tear making it reliable.
-                </p>
-              </div>
-              <div className="slide">
-                <h4>
-                  Cost-Effective:
+            <div className="container advantages-slider mb-5">
 
-                </h4>
-                <p>
-                  It is relatively affordable, making it a cost-effective choice for large-scale advertising campaigns and outdoor promotions.
-                </p>
+              <div className='row'>
+                <div className="whyList col-md-6">
+                  <div className="whyListItem">
+                    <PersonalVideoIcon className='brand-icon' />
+                    <div className="whyListItemTitle">Durability:</div>
+                    <div className="whyListItemBody">
+                      Designed to withstand harsh environmental conditions like sunlight, rain, wind, and temperature variations without significant wear and tear making it reliable.
+                    </div>
+                  </div>
+
+                  <div className="whyListItem">
+                    <AutoFixHighIcon className='brand-icon' />
+                    <div className="whyListItemTitle">
+                      Cost-Effective:</div>
+                    <div className="whyListItemBody">
+                      It is relatively affordable, making it a cost-effective choice for large-scale advertising campaigns and outdoor promotions.
+                    </div>
+                  </div>
+
+                  <div className="whyListItem">
+                    <MonetizationOnIcon className='brand-icon' />
+                    <div className="whyListItemTitle">
+                      Customizable Sizes:
+                    </div>
+                    <div className="whyListItemBody">
+                      Can be easily cut to the desired dimensions without compromising on quality allowing for customization based on the specific requirements of project.
+                    </div>
+                  </div>
+
+                </div>
+                <div className="whyList col-md-6">
+                  <div className="whyListItem">
+                    <AutoFixHighIcon className='brand-icon' />
+                    <div className="whyListItemTitle">
+                      Reusable:
+                    </div>
+                    <div className="whyListItemBody">
+                      Sometimes can be reused for different events or campaigns, further enhancing their cost-effectiveness.
+                    </div>
+                  </div>
+
+                  <div className="whyListItem">
+                    <AutoFixHighIcon className='brand-icon' />
+                    <div className="whyListItemTitle">
+                      Vibrant Printing:
+                    </div>
+                    <div className="whyListItemBody">
+                      Star flex allows for high-quality, vibrant, and sharp printing, making graphics and text stand out.
+                    </div>
+                  </div>
+
+                  <div className="whyListItem">
+                    <AutoFixHighIcon className='brand-icon' />
+                    <div className="whyListItemTitle">
+                      Versatility:
+                    </div>
+                    <div className="whyListItemBody">
+                      Star flex can be used for various advertising materials, including banners, billboards, and posters.
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="slide">
-                <h4>
-                  Customizable Sizes:
-                </h4>
-                <p>
-                  Can be easily cut to the desired dimensions without compromising on quality allowing for customization based on the specific requirements of project.
-                </p>
-              </div>
-              <div className="slide">
-                <h4>
-                  Reusable:
-                </h4>
-                <p>
-                  Sometimes can be reused for different events or campaigns, further enhancing their cost-effectiveness.
-                </p>
-              </div>
-            </Slider>
+
+            </div>
           </div>
         </div>
       </div>
